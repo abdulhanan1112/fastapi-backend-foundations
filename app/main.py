2ef5f400c99c8bbefecec751cfa5b752b25ff54c
Abdul Hanan <hanansattar533@gmail.com>
2026-07-29 13:13:33 +0500

    sttudent api is updated with nested pydantic models,Field constraints, output contract and HTTP Exception and also readme is updated

--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI, status
 from fastapi import Path,Query
 from enum import Enum
-from pydantic import BaseModel
+from pydantic import BaseModel,Field
+from fastapi import HTTPException
+from typing import Annotated
+
 
 
 class Program(str,Enum):
@@ -10,12 +13,44 @@ class Program(str,Enum):
     Informational_Technology='informational_technology'
 
 
+class Address(BaseModel):
+    city:str = Field(
+        min_length=2,
+        max_length=20
+    )
+    country:str = Field(
+        min_length=2,
+        max_length=20
+    )
+
 # class to create Student
 class StudentCreate(BaseModel):
-    name:str
-    age:int
-    email:str|None = None
+    name:str = Field(
+        min_length=2,
+        max_length=100
+    )
+    age:int = Field(
+        ge=15,
+        le=100
+    )
+    email:str|None = Field(
+        default = None,
+        max_length=120
+    )
     program:Program
+    address:Address            # nested pydantic model
+
+
+# Student Response pydantic model
+class StudentResponse(BaseModel):
+    id : int
+    name : str
+    age : int
+    email : str | None
+    program:Program
+    address:Address
+
+
 
 
 students=[]
@@ -30,13 +65,27 @@ app=FastAPI(
 @app.post(
     '/students',
     status_code=status.HTTP_201_CREATED,
+    response_model=StudentResponse,
     tags=['Students']
 )
 def student(student_data:StudentCreate) -> dict[str,object]:
+    if student_data.email is not None:
+        flag=False
+        for student in students:
+            if student['email'] == student_data.email:
+                flag=True
+                break
+        if flag:
+            raise HTTPException(
+                status_code=status.HTTP_409_CONFLICT,
+                detail='This email is already exist'
+            )
+
     student_id=len(students)+1
     new_student={
         "id":student_id,
-        **student_data.model_dump()
+        **student_data.model_dump(),
+        "internal fields" : "Created by API"
     }
     students.append(new_student)
     return new_student
@@ -45,6 +94,7 @@ def student(student_data:StudentCreate) -> dict[str,object]:
 @app.get(
     '/students',
     status_code=status.HTTP_200_OK,
+    response_model=list[StudentResponse],
     tags=['Students']
 )
 def get_students(
@@ -74,6 +124,7 @@ def get_students(
 @app.get(
     '/students/search',
     status_code=status.HTTP_200_OK,
+    response_model=list[StudentResponse],
     tags=['Students']
 )
 def get_students_by_name(name:Annotated[str,Query(min_length=2,max_length=100)]) -> list:
@@ -84,6 +135,11 @@ def get_students_by_name(name:Annotated[str,Query(min_length=2,max_length=100)])
         for student in result
         if name_smaller in student["name"].lower() 
     ]
+    if len(result) == 0:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail='Student with this name is not exist'
+        )
     return result
 
 
@@ -91,11 +147,18 @@ def get_students_by_name(name:Annotated[str,Query(min_length=2,max_length=100)])
 @app.get(
     '/students/{student_id}',
     status_code=status.HTTP_200_OK,
+    response_model=StudentResponse,
     tags=['Students']
 )
 def get_students(student_id:int) -> dict[str,object]:
-    return students[student_id-1]
+    for student in students:
+        if student["id"] == student_id:
+            return student
 
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="Student with this id is not exist"
+    )
 
 
 
